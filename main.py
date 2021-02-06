b9224548c2f78c4e12727979bc85ce92fd8c4d1d
evaisman-tc <evaisman@tucows.com>
2021-02-06 12:03:46 -0500

    added file uploads to api

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from enum import Enum
-from fastapi import FastAPI, Query, Path, Body
+from fastapi import FastAPI, Query, Path, Body, Form, File, UploadFile
+from fastapi.responses import HTMLResponse
 from typing import List, Optional
+
 from helpers.helper import append_to_item
 from models.item import Image, Item, Offer
 from models.user import UserIn, UserOut, fake_save_user
@@ -20,7 +22,44 @@ fake_items_db = [{"item_name": "Foo"}, {
 
 @app.get("/")
 async def root():
-  return {"message": "Hello World"}
+  content = """
+  <body>
+  <form action="/files/" enctype="multipart/form-data" method="post">
+  <input name="files" type="file" multiple>
+  <input type="submit">
+  </form>
+  <form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+  <input name="files" type="file" multiple>
+  <input type="submit">
+  </form>
+  </body>
+      """
+  return HTMLResponse(content=content)
+
+
+@app.post("/login/")
+async def login(username: str = Form(...), password: str = Form(...)):
+  return {"username": username}
+
+
+@app.post("/file/")
+async def create_file(file: bytes = File(...)):
+  return {"file_size": len(file)}
+
+
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile = File(...)):
+  return {"filename": file.filename}
+
+
+@app.post("/files/")
+async def create_files(files: List[bytes] = File(...)):
+  return {"file_sizes": [len(file) for file in files]}
+
+
+@app.post("/uploadfiles/")
+async def create_upload_files(files: List[UploadFile] = File(...)):
+  return {"filenames": [file.filename for file in files]}
 
 
 @app.get("/items/{item_id}")
